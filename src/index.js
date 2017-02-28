4a480d8e652ab64fa3c6f8a2be4414ae969b2a28
Tiago Amaro <tiagopadrela@gmail.com>
2017-02-28 12:53:05 +0100

    wunderground plugin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 'use strict';
 
+const icon = require('./icon.png')
+
 const plugin = ({term, display, actions}) => {
-  // It is your main plugin function
-  // do something and call display() with your results
+  let match = term.match(/weather\s+(.*)/i)
+
+  if (match) {
+    const openWeather = (location) => {
+      let q = encodeURIComponent(location)
+      actions.open(`https://www.wunderground.com/cgi-bin/findweather/getForecast?query=${q}`)
+      actions.hideWindow()
+    }
+
+    display({
+      icon: icon,
+      title: `Show forecast for ${match[1]}`,
+      onSelect: () => openWeather(match[1])
+    })
+  }
 };
 
 module.exports = {
